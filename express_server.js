8b6944980d4b050aeda8e6feafc604920ca72223
Lyon <lyon.b.lee@gmail.com>
2022-11-29 02:06:26 -0500

    I am very confused

--- express_server.js
+++ express_server.js
@@ -2,7 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
+
+function generateRandomString() {
+  let alphaNumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let randomString = '';
+  let number = 0;
+
+  function randomNumberGenerator() {
+    number = Math.floor(Math.random() * alphaNumeric.length);
+    return number;
+  };
+
+  for (let i = 0; i < 6; i++) {
+    randomString += alphaNumeric.charAt(randomNumberGenerator());
+  }
+
+  return randomString;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,11 +36,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 })
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
